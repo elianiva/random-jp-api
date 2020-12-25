f0742c0e36568536fb05405f6f0aecc5442271ca
elianiva <dicha.arkana03@gmail.com>
2020-12-25 12:36:23 +0700

    fix: don't filter empty stuff

--- script.js
+++ script.js
@@ -9,12 +9,11 @@ const en = fs.readFileSync("./thing3").toString().split("\n")
 
 const thing = kanji
   .map((x, i) => ({
-    kanji: x.trim(),
+    kanji: x.trim() === "" ? kana[i].trim() : x.trim(),
     kana: kana[i].trim(),
     en: en[i].trim(),
   }))
   .filter(x => x.kana != "-------------------")
-  .filter(x => x.kana != "" && x.kanji != "" && x.kana != "")
 
 fs.writeFileSync(
   "./result",
