f38f60d5f2eb7e5da527c5f8f3904d808eaa1819
elianiva <dicha.arkana03@gmail.com>
2020-12-25 12:36:42 +0700

    feat: basic data serving

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/elianiva/random-jp-api
 
 go 1.15
+
+require mvdan.cc/gofumpt v0.0.0-20201217210418-dbaa36cfdf8e // indirect
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
@@ -12,10 +15,41 @@ var PORT = "3000"
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello 世界")
+	n5 := make(map[string][]map[string]string)
+	n5json, err := ioutil.ReadFile("./data/n5.json")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	json.Unmarshal(n5json, &n5)
+
+	mux.HandleFunc("/api/n5/all", func(w http.ResponseWriter, r *http.Request) {
+		jsonResp, err := json.Marshal(n5)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, string(jsonResp))
 	})
 
-	log.Fatal(http.ListenAndServe(":"+PORT, mux))
+	mux.HandleFunc("/api/n5/rand", func(w http.ResponseWriter, r *http.Request) {
+		randIdx := rand.Intn(len(n5["data"]))
+
+		data := map[string]map[string]string{
+			"data": n5["data"][randIdx],
+		}
+
+		jsonResp, err := json.Marshal(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, string(jsonResp))
+	})
+
 	fmt.Println("Server running on " + PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, mux))
 }
